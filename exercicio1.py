78c80f4292406377db22eb6d0420d6977010ff0f
Victor Lima <joaozinhobaodebriga@gmail.com>
2025-10-22 17:15:51 -0300

    Add files via upload
    código finalizado

---
+++ exercicio1.py
@@ -0,0 +1,4 @@
+km = float(input("Quantos quilômetros você percorreu? "))
+gas = float(input("Quantos litros de gasolina você gastou nesse percurso? "))
+gasto = km / gas
+print("Seu consumo de gasolina foi de {} km/L".format(gasto))
